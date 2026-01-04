430452f5153ecbcb6dbbad6ac16d60b2743958a1
Pieter Swinkels <swinkels.pieter@yahoo.com>
2026-01-04 22:27:53 +0100

    Add comment to explain usage of specific Python tree sitter grammar

--- my-package-configs/mpc-treesit.el
+++ my-package-configs/mpc-treesit.el
@@ -9,6 +9,7 @@
   :preface
   ;; for now I only need support for Python
   (setq treesit-language-source-alist
-        '((python "https://github.com/tree-sitter/tree-sitter-python"))))
+        ;; v0.23.6 is the last version that works for Emacs 29.4
+        '((python "https://github.com/tree-sitter/tree-sitter-python" "v0.23.6"))))
 
 (provide 'mpc-treesit)
